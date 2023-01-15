106ae22eb176d82a40233270b146bf3df11a5aa5
Dingan Mkhize <dingan.mkhize@yahoo.com>
2023-01-15 13:12:29 +0000

    new codewars ruby challenge completed

--- 8kyu/ruby_challenges.rb
+++ 8kyu/ruby_challenges.rb
@@ -1,3 +1,5 @@
+#DESCRIPTION:
+
 # This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
 # My solution:
@@ -15,3 +17,24 @@
 # def simple_multiplication(number)
 #   number.even? ? number * 8 : number * 9
 # end
+
+# DESCRIPTION:
+
+# Complete the solution so that it reverses all of the words within the string passed in.
+
+# Words are separated by exactly one space and there are no leading or trailing spaces.
+
+# Example(Input --> Output): "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+# My solution:
+
+# def solution(sentence)
+#   return sentence.split(" ").reverse.join(" ")
+# end
+
+# Best solution:
+
+# def solution(sentence)
+#   sentence.split.reverse.join(" ")
+# end
+
