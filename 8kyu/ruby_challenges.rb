b9712170df5e562a3647ab5bfcdfeda95840b91f
Dingan Mkhize <dingan.mkhize@yahoo.com>
2023-01-15 13:59:19 +0000

    new codewars ruby challenge completed

--- 8kyu/ruby_challenges.rb
+++ 8kyu/ruby_challenges.rb
@@ -38,3 +38,29 @@
 #   sentence.split.reverse.join(" ")
 # end
 
+# DESCRIPTION:
+
+# Your task is to create a function that, given a proper first and last name, will return the correct alias.
+
+# Notes:
+# Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given. See the examples below for further details.
+
+# If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+
+# Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+
+# My solution:
+
+# def alias_gen(first, surname)
+#   FIRST_NAME[first[0].upcase] + " " + SURNAME[surname[0].upcase]
+#     rescue
+#       return "Your name must start with a letter from A - Z."
+#     end
+
+# Best solution:
+
+# def alias_gen first, sur
+#   f = FIRST_NAME[first[0].upcase]
+#   l = SURNAME[sur[0].upcase]
+#   f && l ? "#{f} #{l}" : "Your name must start with a letter from A - Z."
+# end
