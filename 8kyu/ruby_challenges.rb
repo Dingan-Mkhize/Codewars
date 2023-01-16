8d5526afb727fa1ae5e7afacb67be020deb01ff1
Dingan Mkhize <dingan.mkhize@yahoo.com>
2023-01-16 14:32:27 +0000

    new codewars ruby challenge completed

--- 8kyu/ruby_challenges.rb
+++ 8kyu/ruby_challenges.rb
@@ -109,3 +109,38 @@
 # def even_or_odd(number)
 #   number.even? ? "Even" : "Odd"
 # end
+
+# DESCRIPTION:
+
+# Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+# invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+# invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+# invert([]) == []
+# You can assume that all values are integers. Do not mutate the input array/list.
+
+# My solution:
+
+# def invert(list)
+#   list.map {|x| 0 - x}
+# end
+
+# Best solution:
+
+# def invert(list)
+#   list.map { |n| -n }
+# end
+
+# DESCRIPTION:
+
+# Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+# Examples (input -> output)
+# 6, "I"     -> "IIIIII"
+# 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+# My solution:
+
+# def repeat_str (n, s)
+#   s * n
+# end
