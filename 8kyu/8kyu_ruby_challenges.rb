363e6b90beaaef81638dee74a01694c8be0aaa48
Dingan Mkhize <dingan.mkhize@yahoo.com>
2023-01-26 15:06:23 +0000

    new codewars ruby challenge completed

--- 8kyu/8kyu_ruby_challenges.rb
+++ 8kyu/8kyu_ruby_challenges.rb
@@ -264,3 +264,36 @@
 #   num * num
 # end
 
+# How many light sabers do you own?
+
+# DESCRIPTION:
+
+# Inspired by the development team at Vooza, write the function that
+
+# accepts the name of a programmer, and
+# returns the number of lightsabers owned by that person.
+# The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+# Note: your function should have a default parameter.
+
+# For example(Input --> Output):
+
+# "anyone else" --> 0
+# "Zach" --> 18
+
+# My solution:
+
+# def how_many_light_sabers_do_you_own(name="")
+#   return name == "Zach" ? 18 : 0
+# end
+
+# Other solutions:
+
+# def howManyLightsabersDoYouOwn(person="Anyone else")
+#   return 18 if person == "Zach"
+#   return 0
+# end
+
+# def howManyLightsabersDoYouOwn *args
+#     args.last == 'Zach' ? 18 : 0
+# end
