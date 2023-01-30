3307a790d49aa340e9f3f0900ce6bb6ae34b787b
Dingan Mkhize <dingan.mkhize@yahoo.com>
2023-01-30 14:37:23 +0000

    new codewars ruby challenge completed

--- 8kyu/8kyu_ruby_challenges.rb
+++ 8kyu/8kyu_ruby_challenges.rb
@@ -320,3 +320,24 @@
 #   b.inspect
 # end
 
+# Unfinished Loop - Bug Fixing #1
+
+# Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+# My solution:
+
+# def create_array(n)
+#   res=[]
+#   i=1
+#   while i<=n
+#     res+=[i]
+#     i+=1
+#   end
+#   return res
+# end
+
+# Best solution:
+
+# def create_array(n)
+#   Array(1..n)
+# end
