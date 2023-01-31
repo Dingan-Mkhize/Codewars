545404a6366d15b226f36d6fb09661507e5331f6
Dingan Mkhize <dingan.mkhize@yahoo.com>
2023-01-31 10:19:35 +0000

    new codewars ruby challenge completed

--- 8kyu/8kyu_js_challenges.js
+++ 8kyu/8kyu_js_challenges.js
@@ -22,3 +22,20 @@ function mango(quantity, price){
 }
 */
 
+/*
+Unfinished Loop - Bug Fixing #1
+
+Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+My solution:
+
+function createArray(number){
+  var newArray = [];
+  
+  for(var i = 1; i <= number; i++){
+    newArray.push(i);
+  }
+  
+  return newArray;
+}
+*/
