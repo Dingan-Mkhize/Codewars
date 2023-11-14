77597d0aeedeb8eec718f90baa8c36d1329a8344
Dingan Mkhize <dingan.mkhize@yahoo.com>
2023-11-14 10:32:35 +0000

    codewars find the needle in the haystack, solved by index

--- 8kyu/8kyu_ruby_challenges.rb
+++ 8kyu/8kyu_ruby_challenges.rb
@@ -382,3 +382,18 @@
   #merged_array = (a + b).uniq.sort
   #return merged_array
 #end
+
+# Can you find the needle in the haystack?
+
+# Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+# After your function finds the needle it should return a message (as a string) that says:
+
+#"found the needle at position " plus the index it found the #needle, so: 
+
+# My solution:
+
+#def find_needle(haystack)
+  #index = haystack.index("needle")
+  #return "found the needle at position #{index}" if index
+#end
