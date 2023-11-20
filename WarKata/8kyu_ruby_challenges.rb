97dcdae86d5923e6b950184d85e6ee531143b63f
Dingan Mkhize <dingan.mkhize@yahoo.com>
2023-11-20 11:02:48 +0000

    any arrows damaged problem, solved

--- WarKata/8kyu_ruby_challenges.rb
+++ WarKata/8kyu_ruby_challenges.rb
@@ -417,3 +417,30 @@
 #   end
 #   processed_array
 # end
+
+# Check your arrows
+# You have a quiver of arrows, but some have been damaged. The quiver contains arrows with an optional range information (different types of targets are positioned at different ranges), so each item is an arrow.
+
+# You need to verify that you have some good ones left, in order to prepare for battle:
+
+# anyArrows([{range=> 5}, {range=> 10, damaged=> true}, {damaged=> true}])
+
+# If an arrow in the quiver does not have a damaged status, it means it's new.
+
+# The expected result is a boolean, indicating whether you have any good arrows left.
+
+# My solution:
+
+# def any_arrows(arrows)
+#   arrows.each do |arrow|
+#     return true unless arrow.key?('damaged') && arrow['damaged'] == true
+#   end
+
+#   false
+# end
+
+# Better solution:
+
+# def any_arrows(arrows)
+#   arrows.any? { |arrow| !arrow['damaged'] }
+# end
