5a100c3f26afe14237cf8165d07d1feedd9a6035
Dingan Mkhize <dingan.mkhize@yahoo.com>
2023-11-24 12:35:53 +0000

    codewars ruby problem solved grass hopper array

--- WarKata/8kyu_ruby_challenges.rb
+++ WarKata/8kyu_ruby_challenges.rb
@@ -465,6 +465,39 @@
 #   num > 0 ? -num : num
 # end
 
+# Find Mean
 
+# Find the mean (average) of a list of numbers in an array.
+# Information
+
+# To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
+
+# For an example list of 1, 3, 5, 7
+
+# 1. Add all of the numbers
+
+# 1+3+5+7 = 16
+
+# 2. Divide by the number of values in the list. In this example there are 4 numbers in the list.
+
+# 16/4 = 4
+
+# 3. The mean (or average) of this list is 4
+
+# My solution:
+
+# def find_average(nums)
+#   return nil if nums.empty?
+  
+#   sum = nums.reduce(:+)
+#   mean = sum.to_f / nums.length
+#   mean
+# end
+
+# Better solution:
+
+# def find_average(nums)
+#   nums.inject(:+) / nums.length.to_f rescue 0
+# end
 
 
